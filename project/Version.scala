4696052558be0b2f5204dccb359ca42e5f25ecf8
Bardur Arantsson <bardur@scientician.net>
2017-05-05 20:26:44 +0200

    Bump scalajs-react to 1.0.0

--- project/Version.scala
+++ project/Version.scala
@@ -1,8 +1,8 @@
 object Version {
 
-  val react = "15.4.2"
+  val react = "15.5.4"
   val monix = "2.2.4"
   val scalaz = "7.2.11"
-  val scalaJsReact = "1.0.0-RC2"
+  val scalaJsReact = "1.0.0"
 
 }
